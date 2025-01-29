a73cd644055942702430a1be14e38bd0ab13dad5
Hason <91471217+EKEO-Hason@users.noreply.github.com>
2025-01-29 10:45:58 +0100

    enable foreign key

--- backend/connexion.py
+++ backend/connexion.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-def get_db_connection():
+def get_db_connection(enable_foreign_keys=True):
   """
   Établit une connexion à la base de données SQLite spécifiée.
 
@@ -10,6 +10,9 @@ def get_db_connection():
   conn = None
   try:
     conn = sqlite3.connect('./sql/base.db')
+    if enable_foreign_keys:
+      cursor = conn.cursor()
+      cursor.execute('PRAGMA foreign_keys = ON')
   except sqlite3.Error as e:
     print(e)
   return conn
